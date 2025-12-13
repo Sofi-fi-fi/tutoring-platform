110223906d61d415e0dd109cfa2ec40776a4be1a
Sofi <lebedeffasofi@gmail.com>
2025-12-13 23:51:33 +0200

    feat(services/dtos): implement 'Tutor' DTO

---
+++ src/TutoringPlatform/Services/DTOs/Tutor/CreateTutorDto.cs
@@ -0,0 +1,12 @@
+namespace TutoringPlatform.Services.DTOs.Tutor;
+
+public class CreateTutorDto
+{
+	public int? CityId { get; set; }
+	public short YearsExperience { get; set; }
+	public string Education { get; set; } = string.Empty;
+	public bool OnlineAvailable { get; set; }
+	public bool OfflineAvailable { get; set; }
+	public string? AboutMe { get; set; }
+	public string? Address { get; set; }
+}    
---
+++ src/TutoringPlatform/Services/DTOs/Tutor/TutorDto.cs
@@ -0,0 +1,13 @@
+namespace TutoringPlatform.Services.DTOs.Tutor;
+
+public class TutorDto
+{
+	public int TutorId { get; set; }
+	public int? CityId { get; set; }
+	public short YearsExperience { get; set; }
+	public string Education { get; set; } = string.Empty;
+	public string? AboutMe { get; set; }
+	public bool OnlineAvailable { get; set; }
+	public bool OfflineAvailable { get; set; }
+	public string? Address { get; set; }
+}
