d7995adcfdc33e9b85a3ddaca8a4fe5599cb2b51
dirigeants <dirigeants.inc@gmail.com>
2025-12-13 19:32:01 +0200

    fix(services/validations): adjust some namings & remove entire entity object testing

--- tests/TutoringPlatform.Tests/Services/Validators/User/UpdateUserDtoValidatorTests.cs
+++ tests/TutoringPlatform.Tests/Services/Validators/User/UpdateUserDtoValidatorTests.cs
@@ -16,7 +16,7 @@ public class UpdateUserDtoValidatorTests
 	#region FirstName Tests
 
 	[Fact]
-	public void FirstName_WhenEmpty_ShouldHaveValidationError()
+	public void FirstName_WhenEmpty_ResultError()
 	{
 		var model = new UpdateUserDto { FirstName = string.Empty };
 		var result = _validator.TestValidate(model);
@@ -25,7 +25,7 @@ public class UpdateUserDtoValidatorTests
 	}
 
 	[Fact]
-	public void FirstName_WhenNull_ShouldHaveValidationError()
+	public void FirstName_WhenNull_ResultError()
 	{
 		var model = new UpdateUserDto { FirstName = null! };
 		var result = _validator.TestValidate(model);
@@ -33,7 +33,7 @@ public class UpdateUserDtoValidatorTests
 	}
 
 	[Fact]
-	public void FirstName_WhenExceedsMaxLength_ShouldHaveValidationError()
+	public void FirstName_WhenExceedsMaxLength_ResultError()
 	{
 		var model = new UpdateUserDto { FirstName = new string('a', 101) };
 		var result = _validator.TestValidate(model);
@@ -50,7 +50,7 @@ public class UpdateUserDtoValidatorTests
 	[InlineData("Студентік#Студентович")]
 	[InlineData("Studentik Studentovych")]
 	[InlineData("Студентік Студентович")]
-	public void FirstName_WhenContainsInvalidCharacters_ShouldHaveValidationError(string firstName)
+	public void FirstName_WhenContainsInvalidCharacters_ResultError(string firstName)
 	{
 		var model = new UpdateUserDto { FirstName = firstName };
 		var result = _validator.TestValidate(model);
@@ -69,7 +69,7 @@ public class UpdateUserDtoValidatorTests
 	[InlineData("Ла-Студентік")]
 	[InlineData("Studentik-Studentov")]
 	[InlineData("Студентік-Студентов")]
-	public void FirstName_WhenValid_ShouldNotHaveValidationError(string firstName)
+	public void FirstName_WhenValid_ResultOk(string firstName)
 	{
 		var model = new UpdateUserDto
 		{
@@ -87,7 +87,7 @@ public class UpdateUserDtoValidatorTests
 	#region LastName Tests
 
 	[Fact]
-	public void LastName_WhenEmpty_ShouldHaveValidationError()
+	public void LastName_WhenEmpty_ResultError()
 	{
 		var model = new UpdateUserDto { LastName = string.Empty };
 		var result = _validator.TestValidate(model);
@@ -96,7 +96,7 @@ public class UpdateUserDtoValidatorTests
 	}
 
 	[Fact]
-	public void LastName_WhenNull_ShouldHaveValidationError()
+	public void LastName_WhenNull_ResultError()
 	{
 		var model = new UpdateUserDto { LastName = null! };
 		var result = _validator.TestValidate(model);
@@ -104,7 +104,7 @@ public class UpdateUserDtoValidatorTests
 	}
 
 	[Fact]
-	public void LastName_WhenExceedsMaxLength_ShouldHaveValidationError()
+	public void LastName_WhenExceedsMaxLength_ResultError()
 	{
 		var model = new UpdateUserDto { LastName = new string('a', 101) };
 		var result = _validator.TestValidate(model);
@@ -121,7 +121,7 @@ public class UpdateUserDtoValidatorTests
 	[InlineData("Студентович#Студентович")]
 	[InlineData("Studentovych Studentovych")]
 	[InlineData("Студентович Студентович")]
-	public void LastName_WhenContainsInvalidCharacters_ShouldHaveValidationError(string lastName)
+	public void LastName_WhenContainsInvalidCharacters_ResultError(string lastName)
 	{
 		var model = new UpdateUserDto { LastName = lastName };
 		var result = _validator.TestValidate(model);
@@ -138,7 +138,7 @@ public class UpdateUserDtoValidatorTests
 	[InlineData("Студентович-Студентовіє")]
 	[InlineData("MacStudentov")]
 	[InlineData("МакСтудентов")]
-	public void LastName_WhenValid_ShouldNotHaveValidationError(string lastName)
+	public void LastName_WhenValid_ResultOk(string lastName)
 	{
 		var model = new UpdateUserDto
 		{
@@ -156,7 +156,7 @@ public class UpdateUserDtoValidatorTests
 	#region Email Tests
 
 	[Fact]
-	public void Email_WhenEmpty_ShouldHaveValidationError()
+	public void Email_WhenEmpty_ResultError()
 	{
 		var model = new UpdateUserDto { Email = string.Empty };
 		var result = _validator.TestValidate(model);
@@ -165,7 +165,7 @@ public class UpdateUserDtoValidatorTests
 	}
 
 	[Fact]
-	public void Email_WhenNull_ShouldHaveValidationError()
+	public void Email_WhenNull_ResultError()
 	{
 		var model = new UpdateUserDto { Email = null! };
 		var result = _validator.TestValidate(model);
@@ -177,7 +177,7 @@ public class UpdateUserDtoValidatorTests
 	[InlineData("@studentik.com")]
 	[InlineData("something.at.com")]
 	[InlineData("studentik@")]
-	public void Email_WhenInvalidFormat_ShouldHaveValidationError(string email)
+	public void Email_WhenInvalidFormat_ResultError(string email)
 	{
 		var model = new UpdateUserDto { Email = email };
 		var result = _validator.TestValidate(model);
@@ -186,7 +186,7 @@ public class UpdateUserDtoValidatorTests
 	}
 
 	[Fact]
-	public void Email_WhenExceedsMaxLength_ShouldHaveValidationError()
+	public void Email_WhenExceedsMaxLength_ResultError()
 	{
 		var model = new UpdateUserDto { Email = new string('a', 246) + "@email.com" };
 		var result = _validator.TestValidate(model);
@@ -199,7 +199,7 @@ public class UpdateUserDtoValidatorTests
 	[InlineData("test.studentik@domain.co.ua")]
 	[InlineData("firstname.lastname@company.com")]
 	[InlineData("user+tag@test.com")]
-	public void Email_WhenValid_ShouldNotHaveValidationError(string email)
+	public void Email_WhenValid_ResultOk(string email)
 	{
 		var model = new UpdateUserDto
 		{
@@ -217,7 +217,7 @@ public class UpdateUserDtoValidatorTests
 	#region Phone Tests
 
 	[Fact]
-	public void Phone_WhenNull_ShouldNotHaveValidationError()
+	public void Phone_WhenNull_ResultOk()
 	{
 		var model = new UpdateUserDto
 		{
@@ -232,7 +232,7 @@ public class UpdateUserDtoValidatorTests
 	}
 
 	[Fact]
-	public void Phone_WhenEmpty_ShouldNotHaveValidationError()
+	public void Phone_WhenEmpty_ResultOk()
 	{
 		var model = new UpdateUserDto
 		{
@@ -247,7 +247,7 @@ public class UpdateUserDtoValidatorTests
 	}
 
 	[Fact]
-	public void Phone_WhenExceedsMaxLength_ShouldHaveValidationError()
+	public void Phone_WhenExceedsMaxLength_ResultError()
 	{
 		var model = new UpdateUserDto { Phone = new string('1', 21) };
 		var result = _validator.TestValidate(model);
@@ -261,7 +261,7 @@ public class UpdateUserDtoValidatorTests
 	[InlineData("abcdefghij")]
 	[InlineData("(555) 123-4567")]
 	[InlineData("555-123-4567")]
-	public void Phone_WhenInvalidFormat_ShouldHaveValidationError(string phone)
+	public void Phone_WhenInvalidFormat_ResultError(string phone)
 	{
 		var model = new UpdateUserDto { Phone = phone };
 		var result = _validator.TestValidate(model);
@@ -275,7 +275,7 @@ public class UpdateUserDtoValidatorTests
 	[InlineData("+1234567890")]
 	[InlineData("+12345678901234")]
 	[InlineData("15551234567")]
-	public void Phone_WhenValid_ShouldNotHaveValidationError(string phone)
+	public void Phone_WhenValid_ResultOk(string phone)
 	{
 		var model = new UpdateUserDto
 		{
@@ -287,58 +287,6 @@ public class UpdateUserDtoValidatorTests
 		
 		var result = _validator.TestValidate(model);
 		result.ShouldNotHaveValidationErrorFor(x => x.Phone);
-	}
-
-	#endregion
-
-	#region Complete Object Tests
-
-	[Fact]
-	public void Validate_WhenAllFieldsValid_ShouldNotHaveAnyValidationErrors()
-	{
-		var model = new UpdateUserDto
-		{
-			FirstName = "Studentik",
-			LastName = "Studentovych",
-			Email = "studentik@test.com",
-			Phone = "+12345678901"
-		};
-		
-		var result = _validator.TestValidate(model);
-		result.ShouldNotHaveAnyValidationErrors();
-	}
-
-	[Fact]
-	public void Validate_WhenAllFieldsValidWithoutPhone_ShouldNotHaveAnyValidationErrors()
-	{
-		var model = new UpdateUserDto
-		{
-			FirstName = "Studentik",
-			LastName = "Studentovych",
-			Email = "studentik@test.com",
-			Phone = null
-		};
-		
-		var result = _validator.TestValidate(model);
-		result.ShouldNotHaveAnyValidationErrors();
-	}
-
-	[Fact]
-	public void Validate_WhenMultipleFieldsInvalid_ShouldHaveMultipleValidationErrors()
-	{
-		var model = new UpdateUserDto
-		{
-			FirstName = string.Empty,
-			LastName = string.Empty,
-			Email = "invalid-email",
-			Phone = "123"
-		};
-		
-		var result = _validator.TestValidate(model);
-		result.ShouldHaveValidationErrorFor(x => x.FirstName);
-		result.ShouldHaveValidationErrorFor(x => x.LastName);
-		result.ShouldHaveValidationErrorFor(x => x.Email);
-		result.ShouldHaveValidationErrorFor(x => x.Phone);
 	}
 
 	#endregion
