7bca80db4e1bef39b1d134ebae437d0661d6ac70
dirigeants <dirigeants.inc@gmail.com>
2025-12-18 23:24:07 +0200

    fix(repositories): add new data access methods for & remove nonsensical ones

--- src/TutoringPlatform/Repositories/Interfaces/ITeachingLevelRepository.cs
+++ src/TutoringPlatform/Repositories/Interfaces/ITeachingLevelRepository.cs
@@ -5,5 +5,6 @@ namespace TutoringPlatform.Repositories.Interfaces;
 public interface ITeachingLevelRepository : IRepository<TeachingLevel>
 {
     Task<TeachingLevel?> GetByNameAsync(string name);
+    Task<TeachingLevel?> GetByPositionAsync(int position);
     Task<IEnumerable<TeachingLevel>> GetOrderedByPositionAsync();
 }
--- src/TutoringPlatform/Repositories/Interfaces/IUserRepository.cs
+++ src/TutoringPlatform/Repositories/Interfaces/IUserRepository.cs
@@ -1,4 +1,3 @@
-using TutoringPlatform.Models.Enums;
 using TutoringPlatform.Models.Entities;
 
 namespace TutoringPlatform.Repositories.Interfaces;
@@ -7,5 +6,4 @@ public interface IUserRepository : IRepository<User>
 {
 	Task<User?> GetByEmailAsync(string email);
 	Task<User?> GetByPhoneAsync(string phone);
-    Task<IEnumerable<User>> GetByTypeAsync(UserType userType);
 }
